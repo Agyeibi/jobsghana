2085375798cd6240ff871411e9b40b3c690ace5f
Samuel Agyeibi <samuelagyeibi15@example.com>
2025-12-15 00:49:21 +0000

    Add gitignore and auto-create DB tables

--- app.py
+++ app.py
@@ -185,4 +185,7 @@ def dashboard():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
